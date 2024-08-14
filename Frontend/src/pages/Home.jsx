4b960489e9cbfb9c8a1aa7c9ac22153265d77dfd
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2024-08-14 19:03:01 +0530

    fixes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,31 @@
+import React, { useState } from "react";
 import Item from "../components/item";
 import Banner from "../components/Banner";
+import CustomizationForm from "./Customization-Form";
 
 const Home = () => {
+  const [customForm, setCustomForm] = useState(false);
   return (
-    <div className="bg-black/80 ">
-
-        <Banner />
-
-      <Item />
+    <div>
+      <div className="flex justify-evenly bg-red-500">
+        <button onClick={() => setCustomForm(false)} className="bg-white">
+          maria readymade aur designers cloth k liye naam bata
+        </button>
+        <button onClick={() => setCustomForm(true)} className="bg-white">
+          maria custom form k liye naam bata
+        </button>
+      </div>
+      {customForm && (
+        <div>
+          <CustomizationForm />
+        </div>
+      )}
+      {!customForm && (
+        <div className="bg-black/80 ">
+          <Banner />
+          <Item />
+        </div>
+      )}
     </div>
   );
 };
