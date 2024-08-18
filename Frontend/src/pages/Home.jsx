01e0664e1398b9b83a0fe95f33040e78db5bd02c
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2024-08-19 03:12:30 +0530

    fixes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,45 @@
+import React, { useEffect, useState } from "react";
 import Item from "../components/item";
 import Banner from "../components/Banner";
+import CustomizationForm from "./Customization-Form";
 
-const Home = () => {
+const Home = ({ customForm }) => {
+  const [showForm, setShowForm] = useState(customForm);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      if (customForm) {
+        const confirmationMessage =
+          "The form is already open. Refreshing will reset it. Do you want to continue?";
+        event.returnValue = confirmationMessage;
+        s;
+        return confirmationMessage;
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [customForm]);
+
+  useEffect(() => {
+    setShowForm(customForm);
+  }, [customForm]);
+
   return (
-    <div className="bg-black/80 ">
-
-        <Banner />
-
-      <Item />
+    <div className="container mx-auto p-4">
+      {showForm ? (
+        <div className="bg-white rounded-lg shadow-lg">
+          <CustomizationForm />
+        </div>
+      ) : (
+        <div className="bg-white rounded-lg shadow-lg">
+          <Banner />
+          <Item />
+        </div>
+      )}
     </div>
   );
 };
